8b8593e6d97e201f307daca5cdec96a35b91b8fe
thiagooliveira <thiagooliveira@digix.com.br>
2020-08-27 20:14:56 -0400

    Instalando font e reduzindo tamanhos de imagnes

--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,7 @@ export default function Page() {
       <Head>
         <title>Drª Luciele Cristofari</title>
         <link rel="icon" href="/favicon.ico" />
+        <link href="https://fonts.googleapis.com/css2?family=Baloo+Tamma+2:wght@500;600;700&display=swap" rel="stylesheet" />
       </Head>
 
       <Header />
