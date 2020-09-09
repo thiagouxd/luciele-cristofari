6c5b1c8dc50a8f5aef1f24e036942c044e4b0813
thiagooliveira <thiagooliveira@digix.com.br>
2020-09-08 21:19:26 -0400

    Retirando tag html

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import Head from 'next/head'
-import { Html } from 'next/document'
 import Header from '../components/header/Header'
 import Home from '../components/home/Home'
 import Especialidades from '../components/sections/especialidades/Especialidades'
@@ -10,7 +9,7 @@ import { descricao } from '../components/sections/sobre-mim/data.json'
 
 export default function Page() {
   return (
-    <Html lang="pt-br">
+    <>
       <Head>
         <title>Drª Luciele Cristofari</title>
         <link rel="icon" href="/favicon.ico" />
@@ -31,6 +30,6 @@ export default function Page() {
       <SobreMim />
       <Depoimentos />
       <Contato />
-    </Html>
+    </>
   )
 }
