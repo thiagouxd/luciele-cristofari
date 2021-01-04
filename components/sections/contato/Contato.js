845d21da3027612c76ad31e59cf3ed7516a75a69
thiagooliveira <thiagooliveira@digix.com.br>
2021-01-04 17:25:43 -0400

    Inserindo novo endereço

--- components/sections/contato/Contato.js
+++ components/sections/contato/Contato.js
@@ -27,9 +27,34 @@ export const Contato = () => (
           <span>51 <strong>3097 3839</strong></span>
         </a>
 
-        <a href="tel:997261372" className="button button_primary button_outline">
+        <a href="tel:51997261372" className="button button_primary button_outline">
           <img src="./whats.svg" alt="" />
           <span>51 <strong>99726 1372</strong></span>
+        </a>
+      </div>
+
+      <h3 className="title title_contato u-margem-superior-grande">
+        925 Independência
+      </h3>
+      <p className="paragraphy paragraphy_contato">
+        <Icon icon="map-pin" className="icon icon_contato" />
+        Av. Independência, 925 - Conjunto 311, Bairro Independência, Porto Alegre / RS
+      <a href="https://goo.gl/maps/ob51X7Ffhn98UdVH6" target="_blank" rel="noreferrer"
+          className="link u-margin-left-middle">
+          <span>Ver no mapa</span>
+          <Icon icon="arrow-up-right" size="16" />
+        </a>
+      </p>
+
+      <div>
+        <a href="tel:5130198900" className="button button_primary button_outline">
+          <Icon icon="phone" className="icon icon_contato" />
+          <span>51 <strong>3019 8900</strong></span>
+        </a>
+
+        <a href="tel:51998406533" className="button button_primary button_outline">
+          <img src="./whats.svg" alt="" />
+          <span>51 <strong>99840 6533</strong></span>
         </a>
       </div>
 
